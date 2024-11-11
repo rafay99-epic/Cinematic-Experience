61c43c1215c49e711ed9cd0924183ae5b1759c1c
rafay99-epic <99marafay@gmail.com>
2024-11-12 03:48:56 +0500

    logo update & 404 page Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,8 @@ import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
 import rehypeExternalLinks from "rehype-external-links";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-template.netlify.app",
   integrations: [mdx(), svelte(), tailwind(), react()],
